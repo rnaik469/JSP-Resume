f815157923b5bbc194653b2b62ea15ac08c8b00b
rnaik469 <rnaik469@gmail.com>
2018-08-07 23:49:16 +0530

    Add files via upload

---
+++ WebContent/output.jsp
@@ -0,0 +1,58 @@
+<%@page import="java.util.Date"%>
+<%@page import="in.co.cg.servlet.model.resume.Resume"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<% Resume resume=(Resume)request.getAttribute("ResumeModel"); %>
+<head>
+
+<meta charset="ISO-8859-1">
+<title><%=resume.getfName()%> <%=resume.getlName()%> Resume</title>
+
+<style>
+body{
+background-color: black;}
+.resume{
+padding:50px;
+background-color: white;
+margin-left: 20%;
+margin-right: 20%;
+}
+.details{
+font-size: small;
+}
+h3{
+
+text-align: center;}
+</style>
+</head>
+<body>
+
+<div class="resume">
+<div class="details"><%= resume.getArrayImageSource()%></div>
+<h2><%=resume.getfName()%> <%=resume.getlName()%></h2> 
+<div class="details"><%= resume.getAddress()%></div>
+<div class="details"><%= resume.getGender()%></div>
+<div class="details"><%= resume.getEmail()%></div>
+<div class="details"><%= resume.getContactNo()%></div>
+<div class="details"><%= resume.getWebsite()%></div>
+<h3> Summary</h3>
+<p><%= resume.getSummary() %></p>
+<hr>
+<h3> Highest Qualification</h3>
+<p><%= resume.gethQualification() %></p>
+<hr>
+<h3> Skills</h3>
+<%String skill[]=resume.getSkills();
+for(String ski:skill)
+	out.println("<li>"+ski+"</li>"); %>
+	<hr>
+<h3> Hobbies</h3>
+<%String hobb[]=resume.getHobbies();
+for(String hob:hobb)
+	out.println("<li>"+hob+"</li>"); %>
+<div class="currdate"> <%Date date=new Date();%><%=date%></div>
+</div>
+</body>
+</html>
---
+++ src/in/co/cg/servlet/contoller/resume/ResumeController.java
@@ -0,0 +1,50 @@
+package in.co.cg.servlet.contoller.resume;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import in.co.cg.servlet.model.resume.Resume;
+
+/**
+ * @author Rohit Naik
+ *
+ */
+@WebServlet("/resume")
+public class ResumeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+  
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	protected void doPos(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Resume resume=new Resume();	//Instantiating Class Resume
+		
+		//Set parameters from html page to setters
+		resume.setAddress(request.getParameter("Address"));
+		resume.setfName(request.getParameter("fName").toUpperCase());
+		resume.setContactNo(request.getParameter("contact"));
+		resume.setDob(request.getParameter("dob"));
+		resume.setlName(request.getParameter("lName").toUpperCase());
+		resume.sethQualification(request.getParameter("HQValue"));
+		resume.setSkills(request.getParameterValues("skills"));
+		resume.setSummary(request.getParameter("desc"));
+		resume.setWebsite(request.getParameter("weburl"));
+		resume.setEmail(request.getParameter("email"));
+		resume.setGender(request.getParameter("gender"));
+		resume.setHobbies(request.getParameterValues("hobbies"));
+		resume.setArrayImageSource(request.getParameter("photo"));
+		request.setAttribute("ResumeModel", resume);
+		
+		//Request for output.jsp
+		RequestDispatcher dispatcher=request.getRequestDispatcher("output.jsp");
+		dispatcher.forward(request, response);
+
+	}
+
+}
